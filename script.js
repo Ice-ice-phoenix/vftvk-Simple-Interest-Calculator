bd161851107b4bb199276fd258ac9b2c095a6b80
Ice-ice-phoenix <104966733+Ice-ice-phoenix@users.noreply.github.com>
2022-05-05 07:20:04 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+
+var s= document.getElementById("ll");
+ var r = document.getElementById("rate")
+    r.addEventListener("change",ff);
+    function ff(){
+         var r = document.getElementById("rate").value;
+         
+    s.innerText=r +"%";
+}
 function compute()
 {
-    p = document.getElementById("principal").value;
+ var   p = document.getElementById("principal").value;
+    var y =document.getElementById("years").value;
+    var r = document.getElementById("rate").value;
+    
+    var result = p*y*r/100;
+    var out= document.getElementById("result");
+    out.innerHTML="if you depost"+" "+p+"<br>" +"at an interest rate of"+" "+ 
+    r+"<br>"+"you will recieve the amonnt"+" "+result+"<br>"+ " in the year" +" " +y;
+    console.log("i am running",result);
     
 }
         
