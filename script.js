e08f95d1ef1478376d0277f0ff557b9dd385f20a
Ice-ice-phoenix <104966733+Ice-ice-phoenix@users.noreply.github.com>
2022-05-06 09:21:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,8 @@ function compute()
     var result = p*y*r/100;
     var out= document.getElementById("result");
     out.innerHTML="if you depost"+" " +"<span class='var'>" +p+ "</span>"+ "<br>" +"at an interest rate of"+" "+ 
-    r+  "<br>"+"you will recieve the amonnt"+" "+result+"<br>"+ " in the year" +" " +y;
+    "<span class='var'>"+ r+ "</span>"+  "<br>"+"you will recieve the amonnt"+" " 
+     +"<span class='var'>"+result+"</span>"+"<br>"+ " in the year" +" "+"<span class='var'>" +y+"</span>";
     console.log("i am running",result);
     
 }
